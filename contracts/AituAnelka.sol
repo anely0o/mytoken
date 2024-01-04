dfb453ae2e66718e1307b31dca4a2f9db8a21022
anely0o <129706883+anely0o@users.noreply.github.com>
2024-01-05 00:37:04 +0600

    Update AituAnelka.sol
    wrong version

--- contracts/AituAnelka.sol
+++ contracts/AituAnelka.sol
@@ -1,14 +1,1 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.6.12 <0.9.0;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract  AituAnelkaToken is ERC20, Ownable {
-  constructor() ERC20("AituAnelka", "UNF") {
-    _mint(msg.sender, 2000*(10**decimals()));
-  }
-  function getLatestTransactionTimestamp() public view returns (uint256){
-    require(block.number>0, "no transactions yet");
-    return block.timestamp;
-  }
-  }     
